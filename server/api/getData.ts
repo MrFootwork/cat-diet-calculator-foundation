dcc35931853665bf15a8ed7724d828e8d32f4713
Mr Footwork <pandau.ting@gmail.com>
2022-05-19 22:45:44 +0200

    renamed MongoDB collection dry -> FoodBrands

--- server/api/getData.ts
+++ server/api/getData.ts
@@ -12,37 +12,6 @@ export default async (req: IncomingMessage, res: ServerResponse) => {
 	res.end()
 }
 
-async function fetchMongo() {
-	const uri = process.env.MONGODB_URI
-	const mongoClient: MongoClient = new MongoClient(uri)
-
-	try {
-		await mongoClient.connect()
-		const db = mongoClient.db('food')
-		const collectionList = await db.listCollections().toArray()
-		const collectionNames = collectionList.map(obj => obj.name)
-		console.log('collections: ', collectionNames)
-		const allFoodBrands = collectionNames.map(async function (foodType) {
-			console.log(foodType)
-			let foodBrandFromDB = []
-			try {
-				foodBrandFromDB = await db.collection(foodType).find({}).toArray()
-			} catch (error) {
-				console.error('could not map food brands. ', error)
-			}
-			foodBrandFromDB['foodType'] = foodType
-			console.log('getData: 35: ', foodBrandFromDB)
-			return foodBrandFromDB
-		})
-		console.log(allFoodBrands)
-		return await allFoodBrands
-	} catch (e) {
-		console.error('could not read from database. ', e)
-	} finally {
-		await mongoClient.close()
-	}
-}
-
 async function fetchSingle() {
 	const uri = process.env.MONGODB_URI
 	const mongoClient: MongoClient = new MongoClient(uri)
@@ -51,7 +20,7 @@ async function fetchSingle() {
 		await mongoClient.connect()
 		const db = mongoClient.db('food')
 		// FIXME rename collection dry
-		const dry = await db.collection('dry').find({}).toArray()
+		const dry = await db.collection('FoodBrands').find({}).toArray()
 		return dry
 	} catch (e) {
 		console.error('could not read from database. ', e)
